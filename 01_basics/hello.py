e225deb8944d9fbf22e130f34a58f4eb8bce792f
kelvinokumu <oumakelvinokumu.gmail.com>
2023-01-24 08:51:16 +0300

    python arithmetic operators

---
+++ 01_basics/hello.py
@@ -0,0 +1,7 @@
+print("Hello world \n")
+
+print("Product tax Check")
+price = float(input("Enter the price "))
+taxRate = float(input("Enter the tax rate "))
+tax = price * taxRate/100
+print("The amount of tax = %0.2f" %(tax))
